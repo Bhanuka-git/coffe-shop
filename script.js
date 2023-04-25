8aa5805efa30e8e40396813dd235c1f08ced6105
Bhanuka-git <minuwanbk96.edu@gmail.com>
2023-04-25 22:16:23 +0530

    first coffee

---
+++ script.js
@@ -0,0 +1,16 @@
+function coffeeDetails(){
+    window.location.href='https://www.webstaurantstore.com/article/397/types-of-coffee-drinks.html'
+}
+
+function pastryDetails(){
+    window.location.href='https://www.taste.com.au/baking/articles/17-sweet-pastries-to-make-your-mouth-water/w9rjjvv3'
+}
+
+function smoothieDetails(){
+    window.location.href='https://www.bbcgoodfood.com/recipes/collection/smoothie-recipes'
+}
+
+function soupDetails(){
+    window.location.href='https://www.tastingtable.com/976762/types-of-soup-and-what-makes-them-unique/'
+}
+
